aa8f2b83fd618f1f4664cbd98b2eb916d780a6fb
Phillip Burgess <paintyourdragon@dslextreme.com>
2021-09-20 20:11:24 -0700

    Fix speckles in LED glasses rings example

--- examples/is31fl3741_glassesrings.py
+++ examples/is31fl3741_glassesrings.py
@@ -11,7 +11,8 @@ glasses = LED_Glasses(board.I2C(), allocate=adafruit_is31fl3741.MUST_BUFFER)
 wheeloffset = 0
 while True:
     for i in range(24):
-        glasses.right_ring[i] = colorwheel(i / 24 * 255 + wheeloffset)
-        glasses.left_ring[23 - i] = colorwheel(i / 24 * 255 + wheeloffset)
+        hue = colorwheel(i * 256 // 24 + wheeloffset)
+        glasses.right_ring[i] = hue
+        glasses.left_ring[23 - i] = hue
     glasses.show()
     wheeloffset += 10
