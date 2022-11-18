1216e9c72d2034fac9b03a8c930bc40b12ba30a5
dherrada <dylan.herrada@adafruit.com>
2022-11-18 13:05:25 -0500

    Added commented out board.STEMMA_I2C with explanation

--- examples/is31fl3741_rgbswirl.py
+++ examples/is31fl3741_rgbswirl.py
@@ -7,7 +7,9 @@ from rainbowio import colorwheel
 from adafruit_is31fl3741.adafruit_rgbmatrixqt import Adafruit_RGBMatrixQT
 import adafruit_is31fl3741
 
-is31 = Adafruit_RGBMatrixQT(board.I2C(), allocate=adafruit_is31fl3741.PREFER_BUFFER)
+i2c = board.I2C()  # uses board.SCL and board.SDA
+# i2c = board.STEMMA_I2C()  # For using the built-in STEMMA QT connector on a microcontroller
+is31 = Adafruit_RGBMatrixQT(i2c, allocate=adafruit_is31fl3741.PREFER_BUFFER)
 is31.set_led_scaling(0xFF)
 is31.global_current = 0xFF
 # print("Global current is: ", is31.global_current)
